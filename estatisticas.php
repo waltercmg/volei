4eae4957a4af9d8065eda86b1d48482a4bcc97c2
waltercmg <waltercmg@gmail.com>
2017-09-12 09:01:26 -0300

    Update estatisticas.php

--- estatisticas.php
+++ estatisticas.php
@@ -2,52 +2,62 @@
 <?php
 include "util.php";
 
-$torneios = getEstatisticasDados();
-
+$torneios = getEstatisticasDadosObj();
 $label = "";
-foreach(array_keys($torneios) as $dt_torneio){
-    //echo "<tr><td colspan=4>TORNEIO ".$dt_torneio."</td></tr>";
-    $dt = $dt_torneio;
-    foreach(array_keys($torneios[$dt_torneio]) as $jogadores){
-        $vitorias = $torneios[$dt_torneio][$jogadores][0]+0;
-        $derrotas = $torneios[$dt_torneio][$jogadores][1]+0;
-        $aprov = number_format($vitorias/($vitorias + $derrotas),2);
-        
-        if($der != "")
-            $der .= "," . $derrotas;
-        else
-            $der = $derrotas ;
-            
-        if($vit != "")
-            $vit .= "," . $vitorias;
-        else
-            $vit = $vitorias ;
-        
-        if($apr != "")
-            $apr .= "," . $aprov;
-        else
-            $apr = $aprov ;
-        
-        if($label != "")
-            $label .= ",'" . $jogadores . "'";
-        else
-            $label = "'" . $jogadores . "'";
-    }
-    break;
-}
+/*echo "<br>DATA 0 :".$torneios[0]->data;
+echo "<br>DATA 1 :".$torneios[1]->data;
+echo "<br>DATA 2 :".$torneios[2]->data;
 
+echo "<br>QT 0 :".$torneios[0]->qtPartidas;
+echo "<br>QT 1 :".$torneios[1]->qtPartidas;
+echo "<br>QT 2 :".$torneios[2]->qtPartidas;*/
 
 $arrayJogadores = array();
-foreach(array_keys($torneios) as $dt_torneio){
-    foreach(array_keys($torneios[$dt_torneio]) as $jogadores){
-        $vitorias = $torneios[$dt_torneio][$jogadores][0]+0;
-        $derrotas = $torneios[$dt_torneio][$jogadores][1]+0;
+
+for($i=0;$i<count($torneios);$i++){
+    $torneio = $torneios[$i];
+    $data[$i] = $torneio->data;
+    foreach(array_keys($torneio->jogadores) as $jogadores){
+        $vitorias = $torneio->jogadores[$jogadores][0]+0;
+        $derrotas = $torneio->jogadores[$jogadores][1]+0;
+        $aprov = number_format($vitorias/($vitorias + $derrotas),2);
+
         $arrayJogadores[$jogadores][0]+=$vitorias;
         $arrayJogadores[$jogadores][1]+=$derrotas;
+
+
+        if($qtPartidasJog[$i] != "")
+            $qtPartidasJog[$i] .= "," . ($derrotas + $vitorias);
+        else
+            $qtPartidasJog[$i] =  ($derrotas + $vitorias);
+        
+        if($label[$i] != "")
+            $label[$i] .= ",'" . $jogadores . "'";
+        else
+            $label[$i] = "'" . $jogadores . "'";
+        
+        if($qtPartidasDia[$i] != "")
+            $qtPartidasDia[$i] .= ",'" . $torneio->qtPartidas . "'";
+        else
+            $qtPartidasDia[$i] = "'" . $torneio->qtPartidas . "'";
+        
+        if($qtVitoriasJog[$i] != "")
+            $qtVitoriasJog[$i] .= ",'" . $vitorias . "'";
+        else
+            $qtVitoriasJog[$i] = "'" . $vitorias . "'";
+        
+        if($qtDerrotasJog[$i] != "")
+            $qtDerrotasJog[$i] .= ",'" . $derrotas . "'";
+        else
+            $qtDerrotasJog[$i] = "'" . $derrotas . "'";
+            
+        if($aprovJog[$i] != "")
+            $aprovJog[$i] .= ",'" . $aprov . "'";
+        else
+            $aprovJog[$i] = "'" . $aprov . "'";    
     }
 }
 
-$label_acumulado = "";
 foreach(array_keys($arrayJogadores) as $jogador){
     $vitorias = $arrayJogadores[$jogador][0];
     $derrotas = $arrayJogadores[$jogador][1];
@@ -79,34 +89,45 @@ foreach(array_keys($arrayJogadores) as $jogador){
 
 <head>
 <script src="Chart.js"></script>
+<link rel="stylesheet" type="text/css" href="estilo.css">
+
 </head>
 
 
-<div class="chart-container" style="position: relative; height:20vh; width:80vw">
-    <canvas id="myChart3"></canvas>
-</div>
+<table width=100%>
+<tr><td>
+    <canvas id="qtPartidasDia"></canvas>
+</td></tr>
+<tr><td>
+    <canvas id="vitDerDia"></canvas>
+</td></tr>
+<tr><td>
+    <canvas id="vitDerAcum"></canvas>
+</td></tr>
+<tr>
+    <td align=center>
+        <input type="button" class="botaoMenu" onclick="location.href = 'index.php'" value="MENU">
+    </td>
+</tr>
+</table>
 <br><br>
+
 <script>
-var ctx = document.getElementById("myChart3").getContext('2d');
+var ctx = document.getElementById("qtPartidasDia").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: [<?=$label?>],
+        labels: [<?=$label[0]?>],
         datasets: [
         {
-            label: '# Vitorias',
-            data: [<?=$vit?>],
+            label: '# Partidas por jogador',
+            data: [<?=$qtPartidasJog[0]?>],
             backgroundColor: 'rgba(54, 162, 235, 0.8)',
             borderWidth: 1
         },    
         {
-            label: '# Derrotas',
-            data: [<?=$der?>],
-            backgroundColor: 'rgba(255, 99, 132, 0.8)',
-            borderWidth: 1
-        }, {
-            label: 'Aproveitamento',
-            data: [<?=$apr?>],
+            label: '# Partidas do dia',
+            data: [<?=$qtPartidasDia[0]?>],
             borderWidth: 1,
             type: 'line'
         }
@@ -115,20 +136,69 @@ var myChart = new Chart(ctx, {
     options: {
         title: {
             display: true,
-            text: 'Torneio do dia <?=$dt?>',
+            text: 'Quantidade de Partidas em <?=$data[0]?>',
             fontSize: 20
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    stepSize: 1
+                }
+            }]
         }
     }
 });
 </script>
-<br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br>
-<br><br><br><br><br><br><br><br><br><br>
-<div class="chart-container" style="position: relative; height:20vh; width:80vw">
-    <canvas id="myChart4"></canvas>
-</div>
+
+
 <script>
-var ctx = document.getElementById("myChart4").getContext('2d');
+var ctx = document.getElementById("vitDerDia").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [<?=$label[0]?>],
+        datasets: [
+        {
+            label: '# Vitorias',
+            data: [<?=$qtVitoriasJog[0]?>],
+            backgroundColor: 'rgba(54, 162, 235, 0.8)',
+            borderWidth: 1
+        },    
+        {
+            label: '# Derrotas',
+            data: [<?=$qtDerrotasJog[0]?>],
+            backgroundColor: 'rgba(255, 99, 132, 0.8)',
+            borderWidth: 1
+        }, {
+            label: 'Aproveitamento',
+            data: [<?=$aprovJog[0]?>],
+            borderWidth: 1,
+            type: 'line'
+        }
+        ]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Resumo do dia <?=$data[0]?>',
+            fontSize: 20
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0,
+                    stepSize: 1
+                }
+            }]
+        }
+    }
+});
+</script>
+
+<script>
+
+var ctx = document.getElementById("vitDerAcum").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -163,3 +233,4 @@ var myChart = new Chart(ctx, {
     }
 });
 </script>
+
