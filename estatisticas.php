6c5e98a32a6d591058df6a7bf390c8550b6555e4
waltercmg <waltercmg@gmail.com>
2022-01-04 17:51:33 -0300

    Update estatisticas.php

--- estatisticas.php
+++ estatisticas.php
@@ -1,9 +1,9 @@
 
 <?php
 include "util.php";
-echo "get estatisticas";
+//echo "get estatisticas";
 $torneios = getEstatisticasDadosObj();
-echo "OK get estatisticas";
+//echo "OK get estatisticas";
 $label = "";
 /*echo "<br>DATA 0 :".$torneios[0]->data;
 echo "<br>DATA 1 :".$torneios[1]->data;
