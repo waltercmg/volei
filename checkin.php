5cc7196ee520cccb5f9ac3bd835fa9537eb98844
waltercmg <waltercmg@gmail.com>
2023-02-01 10:34:08 -0300

    Update checkin.php
    Inclusão do GA

--- checkin.php
+++ checkin.php
@@ -6,6 +6,15 @@ include "util.php";
 <head>
 <link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJTMQ0YWP1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CJTMQ0YWP1');
+</script>
 
 <?php
 
